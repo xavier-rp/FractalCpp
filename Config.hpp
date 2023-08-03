4e9cc2a28f0d37640e0454d24ea72405df1a996c
Xavier <xavier.roy-pomerleau.1@ulaval.ca>
2023-08-03 14:44:15 -0400

    Add Config file to cleanup

--- Config.hpp
+++ Config.hpp
@@ -1,1 +1,21 @@
 #pragma once
+#include <cmath>
+
+static struct Config {
+
+	static constexpr int width = 1240;
+	static constexpr int height = 720;
+	static constexpr double x_min = -2.0;
+	static constexpr double x_max = 1.0;
+	static constexpr double y_min = -1.2;// -1.25;
+	static constexpr double y_max = 1.2;// 1.25;
+
+	static constexpr double c_norm = 0.7885;//0.815;
+	double theta = 2.0;// 2.9830;
+	double c_r{ c_norm * std::cos(theta) };
+	double c_i{ c_norm * std::sin(theta) };
+	static constexpr int max_iter = 1024;
+	static constexpr double max_radius = 2.0;
+
+};
+
--- main.cpp
+++ main.cpp
@@ -8,38 +8,36 @@
 #include "Mandelbrot.hpp"
 #include "Grid.hpp"
 #include "Renderer.hpp"
+#include "WindowUtils.hpp"
+#include "Config.hpp"
 
 const double pi = 3.14159265358979323846;
 
 int main() {
 
-	std::cout << "Running main\n";
+	std::cout << "\n";
 
-	int width = 1240;
-	int height = 720;
-	double x_min = -2.0;
-	double x_max = 1.0;
-	double y_min = -1.2;// -1.25;
-	double y_max = 1.2;// 1.25;
+	Config config{};
 
-	double c_norm = 0.7885;//0.815;
-	double theta = 2.0;// 2.9830;
-	double c_r = c_norm * std::cos(theta);
-	double c_i = c_norm * std::sin(theta);
-	int max_iter = 81;
-	double max_radius = 2.0;
+	Grid grid{ config.x_min, config.x_max, config.y_min, config.y_max, config.width, config.height };
 
-	Grid grid{ x_min, x_max, y_min, y_max, width, height };
+	//Mandelbrot frac{config.max_iter, config.max_radius};//{ -0.8, 0.156, max_iter, 2.0 };
 
-	Mandelbrot mandel{max_iter, max_radius};//{ -0.8, 0.156, max_iter, 2.0 };
+	Julia frac{ config.c_r, config.c_i, config.max_iter, config.max_radius };
 
-	ColorMap color_map{ max_iter, "black_purple_white", false, true, 30 };
+	ColorMap color_map{ config.max_iter, "black_purple_white", false, true, 30 };
 
-	Renderer renderer(mandel, grid, color_map);//(julia, grid, color_map);
+	Renderer renderer(frac, grid, color_map);//(julia, grid, color_map);
 
 	renderer.render_fractal();
 
-	sf::RenderWindow window(sf::VideoMode(width, height), "LimoilouMadness.exe");
+	sf::RenderWindow window(sf::VideoMode(config.width, config.height), "LimoilouMadness.exe");
+
+	sf::RectangleShape zoomBorder(sf::Vector2f(config.width / 8, config.height / 8));
+	zoomBorder.setFillColor(sf::Color(0, 0, 0, 0));
+	zoomBorder.setOutlineColor(sf::Color(255, 255, 255, 128));
+	zoomBorder.setOutlineThickness(1.0f);
+	zoomBorder.setOrigin(sf::Vector2f(zoomBorder.getSize().x / 2, zoomBorder.getSize().y / 2));
 
 
 	while (window.isOpen())
@@ -60,13 +58,25 @@ int main() {
 			case sf::Event::KeyReleased:
 			{
 				if (event.key.scancode == sf::Keyboard::Scan::Right) {
-					theta += (pi / 720);
+					config.theta += (pi / 720);
 				}
 				else {
 					window.close();
 				}
+			} 
+				break;
+
+			case sf::Event::MouseButtonPressed:
+			{
+				if (event.mouseButton.button == sf::Mouse::Left)
+				{
+					
+					zoomx8(grid, event);
+					renderer.render_fractal();
+
+				}
 			}
-			break;
+				break;
 
 			// we don't process other types of events
 			default:
@@ -74,13 +84,14 @@ int main() {
 			}
 		}
 
-		renderer.render_fractal();
+		zoomBorder.setPosition(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y);
 
 		// clear the window with black color
 		window.clear(sf::Color::Black);
 
 		// draw everything here...
 		window.draw(renderer.m_vertices);
+		window.draw(zoomBorder);
 
 		// end the current frame
 		window.display();
